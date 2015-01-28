c21bfc1c0ce8ea007daf98e84822c482712231ae
Soumith Chintala <soumith@fb.com>
2015-01-28 17:28:22 -0500

    fixes for th

--- interpreters/torch.lua
+++ interpreters/torch.lua
@@ -61,16 +61,12 @@ return {
       -- the file path includes unicode characters that need special handling
       local fh = io.open(filepath, "r")
       if fh then fh:close() end
-      if ide.osname == 'Windows' and pcall(require, "winapi")
-      and wfilename:FileExists() and not fh then
-        winapi.set_encoding(winapi.CP_UTF8)
-        filepath = winapi.short_path(filepath)
-      end
-
       script = ('dofile [[%s]]'):format(filepath)
     end
-    local code = ([[xpcall(function() io.stdout:setvbuf('no'); %s end,function(err) print(debug.traceback(err)) end)]]):format(script)
-    local cmd = '"'..torch..'" -e "'..code..'"'
+    
+    -- local code = ([[xpcall(function() io.stdout:setvbuf('no'); %s end,function(err) print(debug.traceback(err)) end)]]):format(script)
+    -- local cmd = '"'..torch..'" -e "'..code..'"'
+    local cmd = 'bash -c "cd $(dirname ' .. filepath .. ') && ' .. torch .. ' ' .. filepath .. ' "' 
     -- CommandLineRun(cmd,wdir,tooutput,nohide,stringcallback,uid,endcallback)
     return CommandLineRun(cmd,self:fworkdir(wfilename),true,false,nil,nil,
       function() ide.debugger.pid = nil end)
