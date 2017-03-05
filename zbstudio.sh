b71f50f8993ae70a240a65a01a9d7c3f6e0e2f00
Paul Kulchenko <paul@kulchenko.com>
2017-03-05 13:16:43 -0800

    Updated MacOS launch script to work with quarantined files on Sierra (#743).

--- zbstudio.sh
+++ zbstudio.sh
@@ -3,8 +3,8 @@
 # Thanks to StackOverflow wiki (http://stackoverflow.com/a/246128)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ $(uname) == 'Darwin' ]]; then
-  (cd "$DIR"; open zbstudio/ZeroBraneStudio.app --args "$@")
+if [[ $(uname) == "Darwin" ]]; then
+  (cd "$DIR"; if [[ $( xattr -pl "com.apple.quarantine" zbstudio 2>&1 ) == com.apple.quarantine:* ]]; then xattr -rd com.apple.quarantine zbstudio bin; fi; open zbstudio/ZeroBraneStudio.app --args "$@")
 else
   case "$(uname -m)" in
 	x86_64) ARCH=x64;;
