e67f82157afc5c1fa25b4499e362151dbd63319b
Paul Kulchenko <paul@kulchenko.com>
2018-10-18 09:41:26 -0700

    Updated indicator handling to work when tabs are quickly closed.
    This was causing an error thrown if one of the closed editor tabs had
    some delayed indicator processing that was pending.

--- src/editor/editor.lua
+++ src/editor/editor.lua
@@ -439,8 +439,17 @@ local delayed = {}
 function IndicateIfNeeded()
   local editor = ide:GetEditor()
   -- do the current one first
-  if editor and delayed[editor] then return editor:IndicateSymbols() end
-  for ed in pairs(delayed) do return ed:IndicateSymbols() end
+  if editor and delayed[editor] then
+    return editor:IndicateSymbols() or next(delayed) ~= nil
+  end
+  local ed = next(delayed)
+  local needmore = false
+  if ide:IsValidCtrl(ed) then
+    needmore = ed:IndicateSymbols()
+  elseif ed then
+    delayed[ed] = nil
+  end
+  return needmore or next(delayed) ~= nil
 end
 
 -- find all instances of a symbol at pos
