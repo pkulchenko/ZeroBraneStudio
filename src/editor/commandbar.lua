pkulchenko/ZeroBraneStudio

commit 35348c00b88215d63398af36671bdaef477e0e3a

Refactored to remove `SetTabIndex` and direct tab index usage.

src/editor/commandbar.lua
@@ -531,7 +531,7 @@ function ShowCommandBar(default, selected)
           local doc = ide:FindDocument(fullPath)
           -- if the document is already opened (not in the preview)
           -- or can't be opened as a file or folder, then close the preview
-          if doc and doc.index ~= pindex
+          if doc and doc:GetTabIndex() ~= pindex
           or not LoadFile(fullPath, preview or nil) and not ide:SetProject(fullPath) then
             if pindex then ClosePage(pindex) end
           end

src/editor/commands.lua
@@ -57,15 +57,11 @@ function LoadFile(filePath, editor, file_must_exist, skipselection)
   end
 
   -- prevent files from being reopened again
-  if (not editor) then
+  if not editor then
     local doc = ide:FindDocument(filePath)
     if doc then
-      if not skipselection and doc.index ~= notebook:GetSelection() then
-        -- selecting the same tab doesn't trigger PAGE_CHANGE event,
-        -- but moves the focus to the tab bar, which needs to be avoided.
-        notebook:SetSelection(doc.index)
-      end
-      return doc.editor
+      if not skipselection then doc:SetActive() end
+      return doc:GetEditor()
     end
   end
 
@@ -359,7 +355,8 @@ function SaveFileAs(editor)
     local filePath = fileDialog:GetPath()
 
     -- check if there is another tab with the same name and prepare to close it
-    local existing = (ide:FindDocument(filePath) or {}).index
+    local doc = ide:FindDocument(filePath)
+    local existing = doc and doc:GetTabIndex() or nil
     local cansave = fn:GetFullName() == filePath -- saving into the same file
        or not wx.wxFileName(filePath):FileExists() -- or a new file
        or ApproveFileOverwrite()

src/editor/editor.lua
@@ -1631,7 +1631,6 @@ function AddEditor(editor, name)
     ide:RemoveDocument(editor)
     return
   else
-    document.index = notebook:GetPageIndex(editor)
     document:SetTabText(name)
     return document
   end

src/editor/gui.lua
@@ -242,12 +242,6 @@ local function createNotebook(frame)
   if ide.wxver >= "2.9.5" then
     notebook:Connect(wxaui.wxEVT_COMMAND_AUINOTEBOOK_END_DRAG,
       function (event)
-        for page = 0, notebook:GetPageCount()-1 do
-          local editor = ide:GetEditor(page)
-          local doc = editor and ide:GetDocument(editor)
-          if doc then doc:SetTabIndex(page) end
-        end
-
         local selection = getTabWindow(event, notebook)
         if selection == wx.wxNOT_FOUND then return end
         -- set the selection on the dragged tab to reset its state

src/editor/package.lua
@@ -221,19 +221,10 @@ end
 function ide:RemoveDocument(ed)
   if not self:IsValidCtrl(ed) or not self.openDocuments[ed:GetId()] then return false end
 
-  local index = self:GetDocument(ed):GetTabIndex()
-  local notebook = ed:GetParent():DynamicCast("wxAuiNotebook")
+  local index, notebook = self:GetDocument(ed):GetTabIndex()
   if not notebook:RemovePage(index) then return false end
 
   self.openDocuments[ed:GetId()] = nil
-
-  for _, document in ipairs(ide:GetDocumentList()) do
-    -- update document index, but only if the document is in the same notebook
-    if notebook == document:GetEditor():GetParent():DynamicCast("wxAuiNotebook")
-    and document:GetTabIndex() > index then
-      document:SetTabIndex(document:GetTabIndex() - 1)
-    end
-  end
 
   local editor = notebook:GetCurrentPage()
   if editor then ide:GetDocument(editor):SetActive() end
@@ -244,7 +235,7 @@ function ide:GetDocuments() return self.openDocuments end
 function ide:GetDocumentList()
   local a = {}
   for _, doc in pairs(self.openDocuments) do table.insert(a, doc) end
-  table.sort(a, function(a, b) return a.index < b.index end)
+  table.sort(a, function(a, b) return a:GetTabIndex() < b:GetTabIndex() end)
   return a
 end
 function ide:GetKnownExtensions(ext)

src/editor/proto.lua
@@ -10,14 +10,16 @@ ide.proto.Document = {__index = {
   GetFileExt = function(self) return GetFileExt(self.fileName) end,
   GetFileModifiedTime = function(self) return self.modTime end,
   GetEditor = function(self) return self.editor end,
-  GetTabIndex = function(self) return self.index end,
-  SetTabIndex = function(self, index) self.index = index end,
+  GetTabIndex = function(self)
+    local notebook = self.editor:GetParent():DynamicCast("wxAuiNotebook")
+    return notebook:GetPageIndex(self.editor), notebook
+  end,
   IsModified = function(self) return self.editor:GetModify() end,
   IsNew = function(self) return self.filePath == nil end,
   IsActive = function(self)
     if not self.editor then return false end
-    local notebook = self.editor:GetParent():DynamicCast("wxAuiNotebook")
-    return notebook:GetSelection() == self.index
+    local index, notebook = self:GetTabIndex()
+    return notebook:GetSelection() == index
   end,
   SetFilePath = function(self, path) self.filePath = path end,
   SetFileName = function(self, name)
@@ -37,31 +39,30 @@ ide.proto.Document = {__index = {
   end,
   SetTabText = function(self, text)
     local modpref = ide.config.editor.modifiedprefix or modpref
-    local notebook = self.editor:GetParent():DynamicCast("wxAuiNotebook")
-    notebook:SetPageText(self.index,
+    local index, notebook = self:GetTabIndex()
+    notebook:SetPageText(index,
       (self:IsModified() and modpref or '')..(text or self:GetTabText()))
     if ide.config.editor.showtabtooltip and ide:IsValidProperty(notebook, "SetPageToolTip") then
-      notebook:SetPageToolTip(self.index, self:GetFilePath() or text or self:GetTabText())
+      notebook:SetPageToolTip(index, self:GetFilePath() or text or self:GetTabText())
     end
     if ide.config.editor.showtabicon then
       if not notebook:GetImageList() then
         notebook:SetImageList(ide:GetProjectTree():GetImageList())
       end
-      notebook:SetPageImage(self.index,
+      notebook:SetPageImage(index,
         ide:GetProjectTree():GetFileImage(self:GetFilePath() or text or self:GetTabText()))
     end
   end,
   GetTabText = function(self)
-    if self.index == nil then return self.fileName end
     local modpref = ide.config.editor.modifiedprefix or modpref
-    local notebook = self.editor:GetParent():DynamicCast("wxAuiNotebook")
-    return notebook:GetPageText(self.index):gsub("^"..q(modpref), "")
+    local index, notebook = self:GetTabIndex()
+    if index == nil then return self.fileName end
+    return notebook:GetPageText(index):gsub("^"..q(modpref), "")
   end,
   SetActive = function(self)
     if not self.editor then return false end
 
-    local notebook = self.editor:GetParent():DynamicCast("wxAuiNotebook")
-    local index = notebook:GetPageIndex(self.editor)
+    local index, notebook = self:GetTabIndex()
     if index and notebook:GetSelection() ~= index then notebook:SetSelection(index) end
 
     self.editor:SetFocus()
@@ -73,9 +74,9 @@ ide.proto.Document = {__index = {
     if ide.osname == "Macintosh" and ide.infocus then ide.infocus = notebook:GetCurrentPage() end
   end,
   Save = function(self) return SaveFile(self.editor, self.filePath) end,
-  Close = function(self) return ClosePage(self.index) end,
+  Close = function(self) return ClosePage((self:GetTabIndex())) end,
   CloseAll = function(self) return CloseAllPagesExcept(-1) end,
-  CloseAllExcept = function(self) return CloseAllPagesExcept(self.index) end,
+  CloseAllExcept = function(self) return CloseAllPagesExcept((self:GetTabIndex())) end,
 }}
 
 ide.proto.Plugin = {__index = {

src/editor/settings.lua
@@ -491,7 +491,6 @@ function SettingsRestoreView()
     local nb = frame.notebook
     local cnt = nb:GetPageCount()
     for i=0,cnt-1 do
-      ide:GetDocument(nb:GetPage(i)):SetTabIndex(i)
       ide:GetDocument(nb:GetPage(i)):SetTabText(nb:GetPageText(i))
     end
   end

commit 42fd7646fd3734bcff0a6fa52d492e3e9e40b3bd

Fixed commandbar navigation for unnamed tabs (broken by 35348c00).

src/editor/commandbar.lua
@@ -676,8 +676,8 @@ function ShowCommandBar(default, selected)
           end
         end
       else
-        for _, doc in pairs(ide:GetDocuments()) do
-          lines[doc:GetTabIndex()+1] = {doc:GetFileName(), doc:GetFilePath(), doc:GetTabIndex()}
+        for _, doc in pairs(ide:GetDocumentList()) do
+          lines[doc:GetTabIndex()+1] = {doc:GetFileName(), doc:GetFilePath(), (doc:GetTabIndex())}
         end
       end
       return lines