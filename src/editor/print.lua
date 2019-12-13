985a27ce5ae5afa1e9e8ad53769bb4609bc1979e
Paul Kulchenko <paul@kulchenko.com>
2019-12-12 21:56:44 -0800

    Updated comments for print calculations.

--- src/editor/print.lua
+++ src/editor/print.lua
@@ -31,7 +31,7 @@ local function printScaling(dc, printOut)
   local page_y    = math.floor(pageSizeMM_y * ppmm_y)
   local pageRect  = wx.wxRect(0, 0, page_x, page_y)
 
-  -- get margins informations and convert to printer pixels
+  -- get margin information and convert to printer pixels
   local top    = math.floor(margin.top    * ppmm_y)
   local bottom = math.floor(margin.bottom * ppmm_y)
   local left   = math.floor(margin.left   * ppmm_x)
@@ -186,8 +186,9 @@ frame:Connect(ID.PRINT, wx.wxEVT_COMMAND_MENU_SELECTED,
 frame:Connect(ID.PRINT, wx.wxEVT_UPDATE_UI, function(event) event:Enable(ide:GetEditorWithFocus() ~= nil) end)
 
 local _, menu, epos = ide:FindMenuItem(ID.EXIT)
--- disable printing on Unix/Linux as it generates incorrect layout (wx2.9.5, wx3.1)
-if ide.osname ~= "Unix" and menu and epos then
+-- disable printing on Unix/Linux as it generates incorrect layout
+-- (fails under wx2.9.5 and wx3.1, but works under wx3.1.3+)
+if (ide.osname ~= "Unix" or ide.wxver >= "3.1.3") and menu and epos then
   -- insert Print-repated menu items (going in the opposite order)
   menu:Insert(epos-1, ID.PAGESETUP, TR("Page Setup..."), "")
   menu:Insert(epos-1, ID.PRINT, TR("&Print..."), TR("Print the current document"))
