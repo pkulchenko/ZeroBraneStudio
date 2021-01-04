43e00301e319bb7126bc5f45286ec96c9e0fa968
Paul Kulchenko <paul@kulchenko.com>
2021-01-03 22:09:13 -0800

    Updated list of window classes to hide (used in some wxlua apps).

--- zbstudio/config.lua
+++ zbstudio/config.lua
@@ -32,11 +32,14 @@ singleinstance = not mac
 unhidewindow = { -- allow unhiding of GUI windows
   -- 1 - show if hidden, 0 - ignore, 2 -- hide if shown
   ConsoleWindowClass = 2,
+  ['Auto-Suggest Dropdown'] = 2,
+  SysShadow = 2,
   -- IUP library window
   ['GDI+ Hook Window Class'] = 2,
   -- ignore the following windows when "showing all"
   IME = 0,
   wxDisplayHiddenWindow = 0,
+  wxTimerHiddenWindow = 0,
   -- window added when Microsoft.Windows.Common-Controls is enabled in the manifest
   tooltips_class32 = 0,
   ['MSCTFIME UI'] = 0,
