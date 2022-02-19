5bff2d11eff05d91e9a0b2b7a1d4450bb0df7667
Phil Hagelberg <phil@hagelb.org>
2022-02-19 08:53:22 -0800

    Update Fennel keywords in lexer.
    Also remove the inherited "longstring" rule from the Lua lexer.
    
    Also add a "kwstring" rule for :this-style string.

--- lualibs/lexers/fennel.lua
+++ lualibs/lexers/fennel.lua
@@ -13,11 +13,17 @@ lex:add_rule('whitespace', token(lexer.WHITESPACE, lexer.space^1))
 
 -- Keywords.
 lex:modify_rule('keyword', token(lexer.KEYWORD, word_match[[
-  % * + - ->> -> -?>> -?> .. . // / : <= < = >= > ^ ~= λ
-  and comment do doc doto each eval-compiler fn for global hashfn if include
-  lambda length let local lua macro macros match not not= or partial quote
-  require-macros set set-forcibly! tset values var when while
+  # % * + - -> ->> -?> -?>> . .. / // : < <= = > >= ?. ^ accumulate
+  and band bnot bor bxor collect comment do doto each eval-compiler fn
+  for global hashfn icollect if import-macros include lambda length
+  let local lshift lua macro macrodebug macros match not not=
+  or partial pick-args pick-values quote require-macros rshift set
+  set-forcibly! tset values var when while with-open ~= λ
 ]]))
+
+-- To regenerate the above:
+-- (table.concat (doto (icollect [k v (pairs (fennel.syntax))]
+--                     (if (or v.special? v.macro?) k)) table.sort) " ")
 
 -- Identifiers.
 local initial = lexer.alpha + S"|$%&#*+-./:<=>?~^_λ!"
@@ -26,6 +32,7 @@ lex:modify_rule('identifier', token(lexer.IDENTIFIER, initial * subsequent^0))
 
 -- Strings.
 lex:modify_rule('string', token(lexer.STRING, lexer.range('"')))
+lex:add_rule('kwstring', token(lexer.STRING, P(":") * subsequent^1))
 
 -- Comments.
 lex:modify_rule('comment', token(lexer.COMMENT, lexer.to_eol(';')))
@@ -33,5 +40,6 @@ lex:modify_rule('comment', token(lexer.COMMENT, lexer.to_eol(';')))
 -- Ignore these rules.
 lex:modify_rule('label', P(false))
 lex:modify_rule('operator', P(false))
+lex:modify_rule('longstring', P(false))
 
 return lex
